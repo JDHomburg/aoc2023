038d59c2a43816b8b508ba7e32d3a29662784b43
BuildTools <unconfigured@null.spigotmc.org>
2023-12-01 22:23:32 +0100

    day01

---
+++ day01/day01.py
@@ -0,0 +1,40 @@
+import re
+
+
+def day01_task1(file_name='./input_task1.txt'):
+    value = 0
+    with open(file_name, 'r') as f:
+        for line in f.readlines():
+            first = re.search('\d', line)
+            last = re.search('\d', line[::-1])
+            tmp = int(first.group() + last.group())
+            value += tmp
+    return value
+
+
+def day01_task2(file_name='./input_task1.txt'):
+    value_map = {'zero': '0',
+                 'one': '1',
+                 'two': '2',
+                 'three': '3',
+                 'four': '4',
+                 'five': '5',
+                 'six': '6',
+                 'seven': '7',
+                 'eight': '8',
+                 'nine': '9'}
+    for i in range(10):
+        value_map[str(i)] = str(i)
+    value = 0
+    with open(file_name, 'r') as f:
+        for line in f.readlines():
+            findings = re.finditer(r'(?=((\d|zero|one|two|three|four|five|six|seven|eight|nine)))', line)
+            finding = [match.group(1) for match in findings]
+            tmp = int(value_map[finding[0]] + value_map[finding[-1]])
+            value += tmp
+    return value
+
+
+if __name__ == '__main__':
+    print(day01_task1())
+    print(day01_task2('./input_task1.txt'))
